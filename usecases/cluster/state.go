4b2555ae0ba023ab632c07392b2178c8cb9a9761
moogacs <contact@m-mooga.com>
2025-09-01 13:08:18 +0200

    revery hostname changes

--- usecases/cluster/state.go
+++ usecases/cluster/state.go
@@ -301,29 +301,18 @@ func (s *State) ClusterHealthScore() int {
 
 // NodeHostname return hosts address for a specific node name
 func (s *State) NodeHostname(nodeName string) (string, bool) {
-	var addr string
+	s.listLock.RLock()
+	defer s.listLock.RUnlock()
 
-	backoffConfig := backoff.NewExponentialBackOff()
-	backoffConfig.InitialInterval = 50 * time.Millisecond
-	backoffConfig.MaxElapsedTime = 1 * time.Second
-
-	err := backoff.Retry(func() error {
-		s.listLock.RLock()
-		defer s.listLock.RUnlock()
-
-		for _, mem := range s.list.Members() {
-			if mem.Name == nodeName {
-				addr = fmt.Sprintf("%s:%d", mem.Addr.String(), mem.Port+1)
-				return nil
-			}
+	for _, mem := range s.list.Members() {
+		if mem.Name == nodeName {
+			// TODO: how can we find out the actual data port as opposed to relying on
+			// the convention that it's 1 higher than the gossip port
+			return fmt.Sprintf("%s:%d", mem.Addr.String(), mem.Port+1), true
 		}
-		return fmt.Errorf("node %s not found", nodeName)
-	}, backoffConfig)
-	if err != nil {
-		return "", false
 	}
 
-	return addr, true
+	return "", false
 }
 
 // NodeAddress is used to resolve the node name into an ip address without the port
